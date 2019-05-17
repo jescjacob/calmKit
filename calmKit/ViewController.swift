de5c0df6e9521ab3f91b33f0ce7ceb00152eb2c3
jessica <chika.workstuff@icloud.com>
2019-05-17 16:15:23 +0700

    Initial Commit

---
+++ calmKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calmKit
+//
+//  Created by Jessica Jacob on 17/05/19.
+//  Copyright © 2019 Jessica Jacob. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
