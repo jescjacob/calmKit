3623715eb6689022048bbb56e8826e3eb3ba9d52
jessica <chika.workstuff@icloud.com>
2019-05-17 17:09:16 +0700

    calmKit day 1

--- calmKit/ViewController.swift
+++ calmKit/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gambarPantai: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = #colorLiteral(red: 0.00546401646, green: 0.6638430953, blue: 0.7191252708, alpha: 1)
+        
     }
 
-
+    
+    @IBAction func sliderWind(_ sender: Any) {
+        
+        
+        
+    }
+    
+    
+    @IBAction func sliderWater(_ sender: Any) {
+    }
+    
+    
+    @IBAction func sliderBird(_ sender: Any) {
+    }
+    
+    @IBAction func sliderMusic(_ sender: Any) {
+    }
 }
 
